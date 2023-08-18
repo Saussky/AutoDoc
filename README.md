8eaea3353998080cff7d8f378f8bdca6106d8f70
Saucy <patrickgardnerbrunton@hotmail.com>
2023-08-18 13:58:08 +1000

    updated readme

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
+## AutoDoc
+Add JS or TSDoc function comments to entire repositories on Github through the use of Github & chatGPT's API.
+Intended use is for legacy code. I do not recommend not writing any comments and leaving it up to AI to do later.
+
 ** Link
-https://auto-doc-tau.vercel.app/
-CURRENTLY NOT WORKING - chatAPI requests take a long time and aren't compatible with Vercel's free limit of 10s per request.
 
 here's a demo link showing how it works, with some errors :)
 https://youtu.be/tXiv9Zf4p0Y
 
-** Explanation
 
-The purpopse of this website is to automatically add TSDoc and JSDoc comments to entire repositories. While it is preferred that you write comments as you go, this often does not happen. Thise website is especially useful for legacy code you didn't write, but still have to use.
-
-It uses chatGPT's 3.5 API to achieve this. GPT 4 would be better but its costs are great.
-
-** 
+** Known Bugs
 Right now there are a few bugs and features that need to be expanded on.
+- API call can take a long time as it has to download Github file then send it through the chatGPT API. If there's a lot of files this takes too long. Will introduce background workers to load them to the user one by one.
 - If you choose to use a github repository in JS, the retry button will retry that code but send it has TS. Easy fix, just haven't worked on this for a while.
-- Styling isn't perfect either.
+- Yet to do any error handling
+- Styling needs work
 - It hasn't been setup with OAUTH for private repositories properly just yet, and there may be issues when pushing to a new GitHub branch because of this
 - Also problems arise if you use JS for some files, then try to use TS after it. Caching error or redux-persist error.
+
--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -4,64 +4,68 @@ dotenv.config();
 
 
 interface Message {
-  role: string;
-  content: string;
+    role: string;
+    content: string;
 }
 
 interface RequestBody {
-  model: string;
-  messages: Message[];
+    model: string;
+    messages: Message[];
 }
 
 function extractCodeBlock(content: string): string {
-  const regex = /```[\s\S]*?```/g;
-  const matches = content.match(regex);
-  if (matches) {
-    return matches.map(match => match.slice(3, -3)).join('\n').trim();
-  }
-  throw new Error('codeblock issue');
+    const regex = /```[\s\S]*?```/g;
+    const matches = content.match(regex);
+
+    if (matches) {
+        const codeBlocks = matches.map(match => match.slice(3, -3)).join('\n');
+        return codeBlocks.trim();
+    } else {
+        console.log(content);
+        throw new Error('codeblock issue');
+    }
 }
 
 export default async function docify(code: string, fileType: 'TS' | 'JS'): Promise<string> {
-  try {
-    const endpoint = 'https://api.openai.com/v1/chat/completions';
-    const requestBody: RequestBody = {
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system", content: `
-            Follow these step-by-step instructions.
+    try {
+        const endpoint = 'https://api.openai.com/v1/chat/completions';
+        const requestBody: RequestBody = {
+            model: "gpt-4-0613", // gpt-3.5-turbo
+            messages: [
+                {
+                    role: "system", content: `
+                    Follow these step-by-step instructions.
 
-            1. Begin your response with three backticks to generate a code block.
-            2. Add all of the imports from the original code at the top of the code block.
-            3. Add the rest of the original code to the code block. Including type and interface definitions.
-
-            4. Identify all of the places where functions are created.
-            5. To all those functions you identified in step 4, find the line directly above them and add ${fileType}Doc comments.
-            6. End your response with three backticks to close the code block.
+                    1. Begin your response with three backticks to generate a code block.
+                    2. Add all of the imports from the original code at the top of the code block.
+                    3. Add the rest of the original code to the code block. Including type and interface definitions.
+                    
+                    4. Identify all of the places where functions are created.
+                    5. To all those functions you identified in step 4, on the line directly above them add ${fileType}Doc comments. In your comment, do not mention that it is a function, just comment what the function does.
+                    6. End your response with three backticks to close the code block.
           `,
-        },
-        { role: "user", content: '```\n' + code + '\n```' }
-      ],
-    };
+                },
+                { role: "user", content: '```\n' + code + '\n```' }
+            ],
+        };
 
-    const response: AxiosResponse = await axios.post(endpoint, requestBody, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-      }
-    });
+        const response: AxiosResponse = await axios.post(endpoint, requestBody, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+            }
+        });
 
-    let tsDocCode = extractCodeBlock(response.data.choices[0].message.content);
-    const firstLine = tsDocCode.trim().split('\n')[0];
+        let tsDocCode = extractCodeBlock(response.data.choices[0].message.content);
+        const firstLine = tsDocCode.trim().split('\n')[0];
 
-    if (firstLine.startsWith('typescript') || firstLine.startsWith('ts')) {
-      tsDocCode = tsDocCode.substring(tsDocCode.indexOf('\n') + 1).trim();
+        if (firstLine.startsWith('typescript') || firstLine.startsWith('ts')) {
+            tsDocCode = tsDocCode.substring(tsDocCode.indexOf('\n') + 1).trim();
+        }
+
+        return tsDocCode;
+    } catch (error) {
+        console.log('Error:', error);
+        throw new Error(`Error adding TSDoc: ${error}`);
     }
-
-    return tsDocCode;
-  } catch (error) {
-    console.log('Error:', error);
-    throw new Error(`Error adding TSDoc: ${error}`);
-  }
 }
